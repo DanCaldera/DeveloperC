41728e2ef814babbfbdaf2edc7799029fc85d6f9
Daniel Caldera <bdcaldera@gmail.com>
2019-11-22 23:24:24 -0700

    untilFork

---
+++ Examples/read_file.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+    FILE * fp = fopen(argv[1], "r");
+    char c;
+
+    while((c=fgetc(fp))!=EOF)
+    {
+        printf("%c",c);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
